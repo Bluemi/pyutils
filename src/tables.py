1126a037061dca5759edf9a99866fbc27c1f2693
Bluemi <bruno.schilling@protonmail.ch>
2024-12-07 12:21:04 +0100

    right align ints

--- src/tables.py
+++ src/tables.py
@@ -5,9 +5,10 @@ from typing import List, Dict, Any, Sequence, Callable, Tuple
 def main():
     table = Table(('A', 'Header with more words', 'c'), header_effect=Effect.underline)
     table.line(A=3.141, c=Effect.yellow('hello'))
-    table.line(A=Effect.cyan(202.71828182), Header_with_more_words=Effect.red('world'))
+    table.line(A=Effect.cyan(202.71828182), Header_with_more_words=Effect.red(1234567))
     table.line(A=Effect.bold(Effect.magenta(2.1)), c=Effect.blue(Effect.underline('blabla')))
-    table.line(A=Effect.green('long string'), Header_with_more_words='something')
+    table.line(A=Effect.green('long string'), Header_with_more_words=32)
+    table.line(A=Effect.green(12345), Header_with_more_words='something')
     print(table)
 
 
@@ -69,7 +70,7 @@ class Table:
         if header_effect is not None:
             headers = [header_effect(h) for h in headers]
         self._string_formatter = StringFormatter(float_precision=float_precision)
-        self._headers: Sequence[Tuple[str, int]] = [self._string_formatter(h, 0) for h in headers]
+        self._headers: Sequence[Tuple[str, int]] = [self._string_formatter(h, 0, 0) for h in headers]
         self._normed_headers: List[str] = [self._norm_header(h) for h in headers]
         self._lines: List[Dict[str, Any]] = []
         self._column_space = column_space
@@ -85,12 +86,15 @@ class Table:
         column_width = {nh: h[1] for nh, h in zip(self._normed_headers, self._headers)}
         sep = ' ' * self._column_space
         longest_float = {nh: 0 for nh in self._normed_headers}
+        longest_int = {nh: 0 for nh in self._normed_headers}
         for line in self._lines:
             for header, value in line.items():
-                str_len = self._string_formatter(value, 0)[1]
+                str_len = self._string_formatter(value, 0, 0)[1]
                 column_width[header] = max(column_width[header], str_len)
                 if Table.has_value_type(value, float):
                     longest_float[header] = max(longest_float[header], str_len)
+                if Table.has_value_type(value, int):
+                    longest_int[header] = max(longest_int[header], str_len)
 
         header_line = sep.join(
             Table.ljust(h, l, column_width[nh]) for nh, (h, l) in zip(self._normed_headers, self._headers)
@@ -103,7 +107,7 @@ class Table:
                 col_width = column_width[header]
                 if header in line:
                     value = line[header]
-                    str_value, str_len = self._string_formatter(value, longest_float[header])
+                    str_value, str_len = self._string_formatter(value, longest_float[header], longest_int[header])
                     str_line.append(Table.ljust(str_value, str_len, col_width))
                 else:
                     str_line.append(' ' * col_width)
@@ -111,7 +115,7 @@ class Table:
         return '\n'.join(lines)
 
     def _norm_header(self, header):
-        header, _header_len = self._string_formatter(header, 0, ignore_effects=True)
+        header, _header_len = self._string_formatter(header, 0, 0, ignore_effects=True)
         return header.replace(' ', '_')
 
     @staticmethod
@@ -128,24 +132,30 @@ class Table:
 class StringFormatter:
     def __init__(self, float_precision: int = 3):
         self._float_fmt_string = f'{{:<.{float_precision}f}}'
-        self.formatters: Dict[Any, Callable[[Any, int, bool], Tuple[str, int]]] = {
+        self.formatters: Dict[Any, Callable[[Any, int, int, bool], Tuple[str, int]]] = {
             float: self._format_float,
+            int: StringFormatter._format_int,
             Effect: self._format_effect
         }
 
-    def __call__(self, value, longest_float_hint: int, ignore_effects=False):
+    def __call__(self, value, longest_float_hint: int, longest_int_hint: int, ignore_effects=False):
         formatter = self.formatters.get(type(value))
         if formatter is not None:
-            return formatter(value, longest_float_hint, ignore_effects)
+            return formatter(value, longest_float_hint, longest_int_hint, ignore_effects)
         str_value = str(value)
         return str_value, len(str_value)
 
-    def _format_float(self, value: float, longest_float_hint: int, _ignore_effects: bool):
+    def _format_float(self, value: float, longest_float_hint: int, _longest_int_hint: int, _ignore_effects: bool):
         str_value = self._float_fmt_string.format(value).rjust(longest_float_hint)
         return str_value, len(str_value)
 
-    def _format_effect(self, effect: Effect, longest_float_hint, ignore_effects: bool):
-        str_content, len_content = self(effect.value, longest_float_hint)
+    @staticmethod
+    def _format_int(value: float, _longest_float_hint: int, longest_int_hint: int, _ignore_effects: bool):
+        str_value = str(value).rjust(longest_int_hint)
+        return str_value, len(str_value)
+
+    def _format_effect(self, effect: Effect, longest_float_hint, longest_int_hint: int, ignore_effects: bool):
+        str_content, len_content = self(effect.value, longest_float_hint, longest_int_hint, ignore_effects)
         if ignore_effects:
             return str_content, len_content
         return effect.color.value + str_content + _Color.ENDC.value, len_content
